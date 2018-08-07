755d96baecd08667f40c22028449a87189c26f55
xzy <1062186337@qq.com>
2018-08-07 17:34:50 +0800

    路由修改

--- src/routeConfig.js
+++ src/routeConfig.js
@@ -1,11 +1,15 @@
-import Un_implants from './Pages/Un_implants';
+import Home from './Pages/Home/Home';
 import Implants from './Pages/Implants';
+import Un_implants from './Pages/Un_implants';
 import Querys from './Pages/Querys/Querys';
 
 export const routeConfig = [{
+  path: '/',
+  component: Home,
+  exact: true
+}, {
   path: '/implants',
   component: Implants,
-  exact: true
 }, {
   path: '/un_implants',
   component: Un_implants
